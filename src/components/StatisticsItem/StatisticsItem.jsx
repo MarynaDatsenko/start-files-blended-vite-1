2c31ec5c25ee6c7f4dd6ca81f74993cf19170d1c
marynad <mskameneva@gmail.com>
2025-02-23 23:13:23 +0200

    first commit

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,13 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './StatisticsItem.module.css';
+
+const StatisticsItem = ({ statData, icon }) => {
+  return (
+    <>
+      {icon}
+      <span className={style.counter}>{statData.total}</span>
+      <p className={style.text}>{statData.title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
